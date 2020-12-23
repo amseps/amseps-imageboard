a67fa06aa5c5664e41aac3d3c2458ab8e57c5930
Joshua <59127653+iambe@users.noreply.github.com>
2020-12-22 16:59:47 -0800

    Update error_404.js

--- amseps/src/components/error_404.js
+++ amseps/src/components/error_404.js
@@ -1,16 +1,6 @@
 import React, {Component} from 'react';
 
-import ViewThread from "./view_thread.component.js";
-
 export default class Error_404 extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    componentDidMount(){
-
-    }
-
     render(){
         return(
             <div>
